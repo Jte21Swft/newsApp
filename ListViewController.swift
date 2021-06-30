f2c80eac10df2ae85fc4cf1dff7961ccae5ed31c
Jte21Swft <fe8hlk3dz7@gmail.com>
2021-06-30 23:41:28 +0900

    tableView

--- ListViewController.swift
+++ ListViewController.swift
@@ -5,4 +5,17 @@
 //  Created by 髙﨑純平 on 2021/06/30.
 //
 
-import Foundation
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+}
